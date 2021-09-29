86206932c3b5940b3676f53f19a48082366085a0
Jiiahhhh <ijlalnurhadi01@gmail.com>
2021-09-29 21:29:06 +0800

    Commit #1

--- app/src/main/java/com/ilal/myservice/MainActivity.kt
+++ app/src/main/java/com/ilal/myservice/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.ilal.myservice
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private var mServiceBound = false
+    private lateinit var mBoundService: MyBoundService
+
+    private val mServiceConnection = object :ServiceConnection {
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            val myBinder = service as MyBoundService.MyBinder
+            mBoundService = myBinder.getService
+            mServiceBound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            mServiceBound = false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val btnStartService = findViewById<Button>(R.id.btnStartService)
+        btnStartService.setOnClickListener {
+            val mStartServiceIntent = Intent(this, MyService::class.java)
+            startService(mStartServiceIntent)
+        }
+
+        val btnStartJobIntentService = findViewById<Button>(R.id.btnStartJobIntentService)
+        btnStartJobIntentService.setOnClickListener {
+            val mStartIntentService = Intent(this, MyJobIntentService::class.java)
+            mStartIntentService.putExtra(MyJobIntentService.EXTRA_DURATION, 5000L)
+            MyJobIntentService.enqueueWork(this, mStartIntentService)
+        }
+
+        val btnStartBoundService = findViewById<Button>(R.id.btnStartBoundService)
+        btnStartBoundService.setOnClickListener {
+            val mBoundServiceIntent = Intent(this, MyBoundService::class.java)
+            bindService(mBoundServiceIntent, mServiceConnection, BIND_AUTO_CREATE)
+        }
+
+        val btnStopBoundService = findViewById<Button>(R.id.btnStopBoundService)
+        btnStopBoundService.setOnClickListener {
+            unbindService(mServiceConnection)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mServiceBound){
+            unbindService(mServiceConnection)
+        }
     }
 }
